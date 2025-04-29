089290ef703da0ae55e1c6af8ba479ba974f297c
Namrata Chandur <chandur.namratanc@gmail.com>
2025-04-29 19:17:01 -0400

    Update EmptyFileNameException.java

--- EmptyFileNameException.java
+++ EmptyFileNameException.java
@@ -1,3 +1,5 @@
+// This class defines a custom exception for handling empty file names 
+
 public class EmptyFileNameException extends Exception 
 {
     public EmptyFileNameException(String message) 
